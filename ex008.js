de612ab35a7a7c52cae43d26c9f772cd51fc417c
henriquetrk <henriquealexandredec@gmail.com>
2023-01-04 21:42:46 -0300

    Update

---
+++ ex008.js
@@ -0,0 +1,6 @@
+var speed = 70
+console.log(`Your car speed is equal to ${speed}Km/h`)
+if (speed > 60) {
+    console.log (`You are above the speed limit`)
+}
+console.log("Always drive using the safety belt")

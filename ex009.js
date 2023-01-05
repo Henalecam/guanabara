6a5dbf36aa6b414f9dc27d293fc9127e873ac4f1
henriquetrk <henriquealexandredec@gmail.com>
2023-01-04 21:47:41 -0300

    Update

---
+++ ex009.js
@@ -0,0 +1,7 @@
+var country = "Italy"
+console.log(`Living in ${country}`)
+if (country != "Brasil" || country != "Brazil"){
+    console.log("You're foreign")
+} else {
+    console.log("You're Brazilian")
+} 

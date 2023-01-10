fe79647d626c9a54fe3d4bff296540c872dcf24f
henriquetrk <henriquealexandredec@gmail.com>
2023-01-10 00:15:06 -0300

    Update

---
+++ JS/aula14/Ex01/script.js
@@ -0,0 +1,38 @@
+function count() {
+    let startn = window.document.getElementById("startn")
+    let finishn = window.document.getElementById("finishn")
+    let stepsn = window.document.getElementById("stepsn")
+    let sum = window.document.getElementById("sum")
+    
+    if (startn.value.lenght == 0 || finishn.value.lenght == 0 || stepsn.value.lenght == 0){
+        sum.innerHTML = "Impossible to count!"
+
+    } else {
+        sum.innerHTML ="Counting: "
+
+        let steps = Number(stepsn.value)
+        
+        let start = Number(startn.value)
+        
+        let finish = Number(finishn.value)
+
+        if (steps <= 0){
+            window.alert("Invalid Step! Step set to '1'")
+            steps = 1
+        }
+
+        if (start < finish) {
+
+            for(let c = start; c <= finish; c =+ steps){
+                
+                sum.innerHTML += `${c} \u{1F449}`
+                
+            }
+        } else{
+            for(let c = start; c >= finish; c -= p){
+                sum.innerHTML += `${c} \u{1F449}`
+            }
+        }
+        sum.innerHTML += `\u{1F3C1}`
+    }
+}
